15a6e92a59f6b252505618b0ae42b270bcc4fe4d
unknown <kwakms821@daum.net>
2020-05-10 12:37:54 +0900

    add item

--- todolist/src/Compontents/Item.js
+++ todolist/src/Compontents/Item.js
@@ -2,9 +2,40 @@ import React from 'react';
 import styled, {css} from 'styled-components';
 import {MdDone, MdDelete} from 'react-icons/md';
 
+const Remove = styled.div`
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    color:#ab193b;
+    font-size:24px;
+    cursor:pointer;
+    &:hover{
+        color:#ff6b6b;
+    }
+`;
+
+const CheckCircle = styled.div`
+    width:10px;
+    height:10px;
+    border-radius:50%;
+    background:#ab183b;
+    font-size:24px;
+    dieplay:flex;
+    align-items:center;
+    justify-content:center;
+    margin-right:20px;
+    cursor:pointer;
+`;
+
+const Text = styled.div`
+    flex:1;
+    font-size:21px;
+    color:#dcdcdc;
+`;
+
 const ItemBox = styled.li`
     display:flex;
-    align-item:center;
+    align-items:center;
     padding:0;
     height:48px;
 
@@ -13,7 +44,13 @@ const ItemBox = styled.li`
 
 function Item(){
     return(
-        <ItemBox></ItemBox>
+        <ItemBox>
+            <CheckCircle />
+            <Text />
+            <Remove>
+                <MdDelete />
+            </Remove>
+        </ItemBox>
     )
 }
 
