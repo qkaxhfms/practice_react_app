ea3dc8a9be643b8c5f370a59f93f656ccb398f30
unknown <kwakms821@daum.net>
2020-05-10 12:04:57 +0900

    start setting

---
+++ todolist/src/Compontents/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalStyles from '../Styles/GlobalStyles';
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './Compontents/App';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -8,3 +8,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+//UI > CONTEXT > FUNCTION
